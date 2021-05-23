a9df17eb86e3b589f82a6579c26eb80aec4dab36
Akanksha-c <64135698+Akanksha-c@users.noreply.github.com>
2021-05-23 15:03:45 +0530

    Initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label={props.label}>
+          {props.img}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.description}</dd>
+    </div>
+  );
+}
+
+export default Entry;
---
+++ src/emojipedia.js
@@ -0,0 +1,109 @@
+const emojipedia = [
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "'m the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 3,
+    emoji: "😂",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "grinning face",
+    meaning:
+      "The happy smiling face is one of the most common emojis and universally applicable: you just want to say hello, express joy or excitement about something or brighten up a short text"
+  },
+  {
+    id: 6,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning:
+      "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo"
+  },
+  {
+    id: 7,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning:
+      "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval. "
+  },
+  {
+    id: 8,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "Emoji is both very sad and distraught, or is dying of laughter. Tears are pouring out of the eyes like a waterfall. Represents injury, pain or defeat. Is also used ironically or something is so funny that you die of laughter."
+  },
+  {
+    id: 9,
+    emoji: "😳",
+    name: "Flushed Face",
+    meaning:
+      "Person is shocked, in an uncomfortable situation or has made a mistake. Expresses shame; something is very embarrassing. Reacts to a compliment or a flattering message that the person did not want to receive."
+  },
+  {
+    id: 10,
+    emoji: "😥",
+    name: "Disappointed but Relieved Face",
+    meaning:
+      "Is relieved that something is over, but also disappointed. Has experienced a stressful situation with an unpleasant outcome, for example, a test. Has gotten out of a difficult or delicate situation, but not without bumps and bruises."
+  },
+  {
+    id: 11,
+    emoji: "😐",
+    name: "Neutral Face",
+    meaning:
+      "Pokerface: Expressionless face with a neutral look. Unimpressed, awkward or indifferent. 	"
+  },
+  {
+    id: 12,
+    emoji: "😴",
+    name: "Sleeping Face",
+    meaning:
+      "Good night and sweet dreams! In comics or cartoons, zZz above the head stands for sleeping characters. The emoji is tired, wants to go to bed or is just about to fall asleep from boredom."
+  },
+  {
+    id: 13,
+    emoji: "😷",
+    name: "Face With Medical Mask",
+    meaning:
+      "The mask is for protection. Either to protect yourself or others from infection. Someone is in the hospital, has to go to the doctor or has caught a disease. Wearing the masks is widely spread in Asia."
+  },
+  {
+    id: 14,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  },
+  {
+    id: 1,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around. "
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 15,
+    emoji: "👻",
+    name: "Ghost",
+    meaning:
+      "The funny-smiling ghost is too cute to frighten others or cause harm. Symbol for Halloween or jokingly for creepy things, e.g. movies."
+  }
+];
+
+export default emojipedia;
